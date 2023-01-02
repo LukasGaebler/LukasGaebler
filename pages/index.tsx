db02c42e5b01c963e1ad6005e94cee0819957753
Lukas Gäbler <lukas.gaebler@gmail.com>
2023-01-02 21:02:05 +0100

    feat: add linkedin icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,42 +2,47 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from "../styles/Home.module.scss"
-import {BiChevronDown} from "react-icons/bi"
 import {Fade} from "react-reveal";
-import Head from 'next/head'
+import linkedIn from "../public/linkedin.png"
 
 const Home: NextPage = () => {
   return (
     <>
-
-      
       <div className={styles.header}>
-        <div className={styles.profilePic}>
-          <Image className={styles.profilePicImage} src={"/cio_talk.jpg"} width={260} height={275}/>
+        <div className={styles.content}>
+          <div className={styles.profilePic}>
+            <Image className={styles.profilePicImage} src={"/cio_talk.jpg"} width={260} height={275} alt="Lukas Gäbler"/>
+          </div>
+          <h1>Lukas Gäbler</h1>
+          <Fade bottom delay={200}>
+            <h2>Software Developer / Consultant</h2>
+          </Fade>
+          <Fade bottom delay={200}>
+            <div className={styles.contactContainer}>
+              <div className={styles.email}>
+                <Link href={"mailto:lukas.gaebler@gmail.com"}>lukas.gaebler@gmail.com</Link>
+              </div>
+              <div className={styles.linkedInLink}>
+                <div className={styles.linkedInLogoContainer}>
+                  <a href="https://www.linkedin.com/in/lukas-gaebler/">
+                    <Image  src={linkedIn} alt="linked in" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Fade>
+          <Fade bottom delay={200}>
+            <p className={styles.skills}>
+              Software Development (Web, Mobile, Desktop)
+            </p>
+            <p className={styles.skills}>
+              React, Angular, C#, Java, Microservices, TDD
+            </p>
+            <p className={styles.skills}>
+              Agile Project Management
+            </p>
+          </Fade>
         </div>
-        <h1>Lukas Gäbler</h1>
-        <Fade bottom delay={200}>
-          <h2>Software Developer / Consultant</h2>
-        </Fade>
-        <Fade bottom delay={200}>
-          <p className={styles.email}>
-            <Link href={"mailto:lukas.gaebler@gmail.com"}>lukas.gaebler@gmail.com</Link>
-          </p>
-        </Fade>
-        <Fade bottom delay={200}>
-          <p className={styles.skills}>
-            Software Development (Web, Mobile, Desktop)
-          </p>
-          <p className={styles.skills}>
-            React, Angular, C#, Java, Microservices, TDD
-          </p>
-          <p className={styles.skills}>
-            Agile Project Management
-          </p>
-        </Fade>
-        {/*<p className={styles.down}>
-          <BiChevronDown></BiChevronDown>
-      </p>*/}
       </div> 
     </>
   )
